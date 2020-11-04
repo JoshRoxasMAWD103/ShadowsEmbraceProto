3f80b6b7365d6cb9e50e3c76e9da20f62ee67dad
JoshRoxas <aaronjoshuaroxas678@gmail.com>
2020-11-05 07:38:29 +0800

    Update ShadowsEmbrace.java

--- ShadowsEmbrace.java
+++ ShadowsEmbrace.java
@@ -1,88 +1,92 @@
-import java.util.Scanner;
 import java.util.Random;
-public class ShadowsEmbrace 
-{
-	Scanner s = new Scanner(System.in);
-	Random r = new Random();
-	String user;
-	String playerWeapon;
-	String role;
-	int chosenRole;
-	int playerHP = 100;
-	int userDamage = r.nextInt(50);
-	String monsters[] = {"Ghoul", "Vampire",};
-	String boss;
-	int randomNumber = r.nextInt(monsters.length);
-	public static void main(String[] args) 
-	{
-		
-		ShadowsEmbrace game = new ShadowsEmbrace();
-		game.playerSetup();
-		game.onStart();
-		game.monsterFight();
-	
-	}
+import java.util.Scanner;
 
-	public void playerSetup() 
+public class Prototype
+{
+	public static void main(String[] args)
 	{
-		System.out.println("---------------------------------------------------------");
-		System.out.println("\nWelcome to Shadows Embrace.");
-		System.out.print("Please enter your name: ");
-		user = s.nextLine();
-		System.out.println();
+		//Variables
 		
-		System.out.println("Please select a class: ");
-		System.out.println("1. Tank (Additional 100 HP) ");
-		System.out.println("2. Warrior (Additional Attack Percentage) ");
-		System.out.println("3. Fighter (Balanced or Combination of Tank and Warrior) ");
-		chosenRole = s.nextInt();
+		//Mostly Strings
+   		Scanner s = new Scanner(System.in);
+  		Random r = new Random();
+   		String user;
+   		String monsters[] = {"Ghoul", "Vampire", "Skeleton",};
+   		int monsterRandomizer = r.nextInt(monsters.length);
+    	String playerChoice;
+    	
+    	//Base Player Stats
+  	int playerhp = 100;
+    	int playerDmg = 50;
+    	
+    	//For heal (future purposes)
+   	int healcount = 3;
+  	final int healamount = 50;
+    	
+    	//Monster stats randomizers and arrays
+   	int monsterHP[] = {25,30,35,40,45,50,};
+  	int monsterDmg[] = {5,10,15,20,25,};
+   	int monstercount = 10;
+   		
+  		{
+  	    System.out.println("------------------------------------------------"); 
+	    System.out.println("Welcome to Shadow's Embrace.");
+	    System.out.print("Your adventure begins here. Please enter a name: ");
+	    user = s.nextLine();
+	    
+	    /*CHOOSE:
+	    System.out.println(user + ", Choose your weapon of choice:");
+	    System.out.println("1.Sword");
+	    System.out.println("2.Gun");
+	    playerChoice = s.nextLine();
 		
-		if (chosenRole == 1)
-		{
-			playerHP = playerHP + 100;
-			role = "Tank";
-			System.out.println("Welcome to Shadows Embrace " + user + " your role is " + role + " you now have additional 100 HP!");
-			System.out.println("Your current hp is " + playerHP);
-			System.out.println();
-		}
-		
-		if (chosenRole == 2)
-		{
-			userDamage = userDamage + 50;
-			role = "Warrior";
-			System.out.println("Welcome to Shadows Embrace " + user + " your role is " + role + " you now have increased damage.");
-		}
-		if (chosenRole == 3)
-		{
-			userDamage = userDamage + 25;
-			playerHP = playerHP + 25;
-			role = "Fighter";
-			System.out.println("Welcome to Shadows Embrace " + user + " your role is " + role + " you now have additional HP and increased damage.");
-		}	
-	}
-	
-	
-	
-	public void onStart()
-	{
-		System.out.println("---------------------------------------------------------");
-		System.out.println("\nWelcome " + user + ", Ah i see that you chose " + role + " Role. Very good Choice! Now lets begin.");
-		System.out.println("You are a man fighting for your love of your life! You must rescue her in order for you to successfully finish this mission!");
+		if (playerChoice.equals("1"))
+			{
+				playerhp += 25;
+				playerDmg -= 10;
+			}
+		else if (playerChoice.equals("2"))
+			{
+				playerhp -= 10;
+				playerDmg += 25;
+			}
+		else
+			{
+				System.out.println("The number you chose is not in the choices!");
+			}*/
+			
+  		}
+  		{
+		//START OF GAME
+	   	System.out.println("------------------------------------------------");
+		System.out.println(user + ", You are a man fighting for your love of your life!");
+		System.out.println("You must rescue her in order for you to successfully finish this mission!");
+		System.out.println("You only have " + healcount + " heals that can restore 50 HP");
+		System.out.println("Never choose run or you will die");
+		System.out.println("(Press ENTER to proceed.)");
 		s.nextLine();
+  		}
+  		
+		/*boolean running = true;
 		
-		System.out.println("But beware, It wont be as easy as it looks like. You may encounter demigod like creatures from the underworld so be careful!");
-	
+		GAME:
+	    while (running)*/
+	    
+		{
+			System.out.println("------------------------------------------------");
+			/*int CurrentMonsterHP = r.nextInt(monsterHP.length);
+			int CurrentMonsterDmg = r.nextInt(monsterDmg.length);*/
+			
+   			int CurrentMonsterHP = r.nextInt(monsterHP.length);
+  			int CurrentMonsterDmg = r.nextInt(monsterDmg.length);
+  			
+			System.out.println("A " + monsters[monsterRandomizer] + " with " + monsterHP[CurrentMonsterHP] + " health and "
+				+ monsterDmg[CurrentMonsterDmg] + " appeared! What do you want to do?");
+			monstercount--;
+		}
+		System.out.println("hindi pa tapos");
 	}
-	
-	public void monsterFight()
-	{
-		System.out.println("A " + monsters[randomNumber] + " appeared!");
-	}
-	
 }
-	
-	
-	
 	
 	
 	
